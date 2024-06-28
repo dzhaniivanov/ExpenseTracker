b93126c2cbb0f323fe9b68e461353bd19c68f7b5
Dzhani <dzhani.ivanov@gmail.com>
2024-06-28 13:36:43 +0300

    header component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-const Home = () => {
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+const Home = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
   return (
     <main>
-      <h1>expense tracker</h1>
+      <h1>Welcome, {user.firstName}</h1>
     </main>
   );
 };
